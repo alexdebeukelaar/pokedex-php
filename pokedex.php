261cf9af8e220e3b47957757193d916eab23df43
Alex de Beukelaar <82021515+alexdebeukelaar@users.noreply.github.com>
2021-05-18 11:34:29 +0200

    worked on php and added my html into my php

--- pokedex.php
+++ pokedex.php
@@ -1,22 +1,86 @@
 <?php
+$pokeUserInputID = 1;
+$pokeUserInputName = 1;
+
+if (isset($_GET["id"])) {
+    $pokeUserInputID = $_GET["id"];
+}
+
+if (isset($_GET["name"])){
+    $pokeUserInputName = $_GET["name"];
+}
+
+
 //Fetching the API
-$api_url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET['search'];
-$json_data = file_get_contents($api_url);
+$pokeRawData = 'https://pokeapi.co/api/v2/pokemon/' .
+/
+
+
+$data = file_get_contents($pokeRawData);
+
 
 
 //all variables needed
-$pokemon_name = $pokemon_data['name'];
-$pokemon_id = $pokemon_data['id'];
-$pokemon_image = $pokemon_data['sprites']['front_default'];
-$pokemon_move1 = $pokemon_moves[0]['move']['name'];
-$pokemon_move2 = $pokemon_moves[1]['move']['name'];
-$pokemon_move3 = $pokemon_moves[2]['move']['name'];
-$pokemon_move4 = $pokemon_moves[3]['move']['name']; 
+$pokeName = $pokeData['name'];
+$pokeID = $pokeData['id'];
+$pokeImage = $pokeData['sprites']['front_default'];
 
-//Displaying the variables
-<?php echo $pokemon_name ?>
-<?php echo $pokemon_id ?>
-<?php echo $pokemon_move1 ?>
-<?php echo $pokemon_move2 ?>
-<?php echo $pokemon_move3 ?>
-<?php echo $pokemon_move4 ?>
+$pokeMove1 = $pokeData['moves'][0]['move']['name'];
+$pokeMove2 = $pokeData['moves'][1]['move']['name'];
+$pokeMove3 = $pokeData['moves'][2]['move']['name'];
+$pokeMove4 = $pokeData['moves'][3]['move']['name'];
+
+
+
+
+
+
+?>
+
+
+<!-- Beginning of the HTML-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pokedex with PHP</title>
+    <link rel="stylesheet" href="style/style.css">
+
+</head>
+
+
+<!-- Structure of the page-->
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="column">
+                
+            </div>
+
+            <div class="column">
+                <div class="image">
+               
+                </div>
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+                <form method="get">
+                    Pokemon id: <input type="text" name="id">
+                    Pokemon name: <input type="text" name="name">
+                    <input type="submit">
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+
+
+</body>
+
+
